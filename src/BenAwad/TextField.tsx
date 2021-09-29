5beeaa8b54c6d20c00679ff7f1e0f3a45497bdf7
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-29 10:30:48 +0200

    Ben Awad showcase optional properties inside interface

--- src/BenAwad/TextField.tsx
+++ src/BenAwad/TextField.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+interface Person {
+	firstName: string;
+	lastName: string;
+}
+
 interface Props {
 	text: string;
+	ok?: boolean;
+	i?: number;
+	fn?: (name: string) => string;
+	obj1?: {
+		property1: string;
+		property2: string;
+	};
+	person?: Person;
 }
 
 const TextField: React.FC<Props> = (props: Props) => {
