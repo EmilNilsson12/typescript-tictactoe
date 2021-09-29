8994adbcd592063ac13ae2af2fbf31ce3205ff57
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-09-29 13:55:51 +0200

    JS Class constructors

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export class Message {
+	title: string;
+	message: string;
+	isSent: boolean;
+
+	constructor(title: string, message: string) {
+		this.title = title;
+		this.message = message;
+		this.isSent = false;
+	}
+}
+
+const message = new Message('Hello', 'World');
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const message1 = { title: undefined, message: undefined };
+	const message2 = { title: undefined, message: undefined };
+	const message3 = { title: undefined, message: undefined };
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<div>
+					{message.title}: {message.message}
+				</div>
+				<div>
+					{message1.title}: {message1.message}
+				</div>
+				<div>
+					{message2.title}: {message2.message}
+				</div>
+				<div>
+					{message3.title}: {message3.message}
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
